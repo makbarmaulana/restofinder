511c9b3d3e259ebfa7eea361e045272a898f1e50
M Akbar Maulana <makbarm.id@gmail.com>
2023-05-12 19:45:08 +0700

    ADD: header & hero component

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,16 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css';
 
-console.log('Hello Coders! :)');
+const nav = document.querySelector('.nav');
+const drawer = document.querySelector('#drawer');
+
+drawer.addEventListener('click', function () {
+  nav.classList.toggle('active');
+});
+
+document.addEventListener('click', function ({ target }) {
+  if (target !== drawer && target !== nav) {
+    nav.classList.remove('active');
+  }
+});
