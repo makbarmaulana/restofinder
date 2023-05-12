f1bb50e1d8f4b279cc6d50dc5c6979df4299e454
M Akbar Maulana <makbarm.id@gmail.com>
2023-05-12 22:31:29 +0700

    ADD: show content restaurants

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,7 @@ import '../styles/responsive.css';
 
 const nav = document.querySelector('.nav');
 const drawer = document.querySelector('#drawer');
+const contents = document.querySelector('.contents');
 
 drawer.addEventListener('click', function () {
   nav.classList.toggle('active');
@@ -13,4 +14,25 @@ document.addEventListener('click', function ({ target }) {
   if (target !== drawer && target !== nav) {
     nav.classList.remove('active');
   }
+});
+
+import('../DATA.json').then(({ default: data }) => {
+  const { restaurants } = data;
+  let contentItem = '';
+
+  restaurants.forEach((restaurant) => {
+    contentItem += `
+    <article class="content__item">
+      <img class="content__thumbnail" src="${restaurant.pictureId}" alt="${restaurant.name}">
+      <div class="content__detail">
+        <p class="content__detail_rating">Rating: ${restaurant.rating} / 5</p>
+        <h3 class="content__detail_name">${restaurant.name}</h3>
+        <p class="content__detail_description">${restaurant.description}</p>
+        <p class="content__detail_city">${restaurant.city}</p>
+      </div>
+    </article>
+    `;
+
+    contents.innerHTML = contentItem;
+  });
 });
